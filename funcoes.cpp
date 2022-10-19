14f69a043b52df6dc4cd7b7f44355d3efc250dca
Gustavo Brendon <104071330+GulinBro@users.noreply.github.com>
2022-10-19 07:36:44 -0300

    Create funcoes.cpp

---
+++ funcoes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+double soma(double a, double b){
+    return a+b;
+}
+double subtracao(double a, double b){
+    return a-b;
+}
+double modulo(double x){
+    if(x>0){
+        return x;
+    }else{
+        return x*-1;
+    }
+}
+double divisao(double a, double b){
+    return a/b;
+}
+double multiplicacao(double a, double b){
+    return a*b;
+}
