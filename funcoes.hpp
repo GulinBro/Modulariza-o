e7f013f53d47affda96d7ce3c66e664b115e08f5
Gustavo Brendon <104071330+GulinBro@users.noreply.github.com>
2022-10-19 07:36:24 -0300

    Create funcoes.hpp

---
+++ funcoes.hpp
@@ -0,0 +1,22 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+#include "funcoes.cpp"
+
+double soma(double a, double b);
+double subtracao(double a, double b);
+double modulo(double x);
+double divisao(double a, double b);
+double multiplicacao(double a, double b);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
